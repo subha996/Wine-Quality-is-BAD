c7d816b0553722f60923c3202ca6d908d110dda2
Subhabrata Nath <ssjournalofficial@gmail.com>
2021-09-30 12:14:31 +0530

    add webapp folder

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -9,7 +9,7 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from get_data import read_params
 import argparse
